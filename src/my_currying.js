8b278295983f2f07620f655b5be725bcb4ffd0e7
Paul Cavanagh <paulcavanagh@Pauls-MacBook-Pro.local>
2017-11-14 10:16:54 +0000

    Currying example

---
+++ src/my_currying.js
@@ -0,0 +1,67 @@
+'use strict'
+/* 
+    currying is a way of constructing functions that allows partial application of a function’s arguments. 
+    What this means is that you can pass all of the arguments a function is expecting and get the result, 
+    or pass a subset of those arguments and get a function back that’s waiting for the rest of the arguments. 
+    It really is that simple
+
+    https://www.sitepoint.com/currying-in-functional-javascript/
+*/
+
+function simpleMethod(){
+    return 'paul';
+}
+
+function greet (greeting,name){
+    return greeting + ", " + name;
+}
+
+function greetCurried(greeting){
+    return function (name){
+        return greeting + ", " + name;
+    }
+}
+
+function greetDeeplyCurried(greeting){
+    return function(separtor){
+        return function(emphasis){
+            return function(name){
+                return greeting+separtor+name+emphasis;
+            };
+        }
+    }
+}
+
+function greeting(greeting, separator, emphasis, name){
+    return greeting+separator+name+emphasis;
+};
+
+function greetDeeplyCurriedFatArrow(greeting){
+    return (separtor) => {
+        return (emphasis) => {
+            return (name)=> {
+                return greeting+separtor+name+emphasis;
+            };
+        };
+    };
+};
+
+function curryIt(uncurried){
+    const parameters = Array.prototype.slice.call(arguments, 1);
+    return function () {
+        return uncurried.apply(this, parameters.concat(
+            Array.prototype.slice.call(arguments, 0)
+        ));
+    };
+};
+
+
+module.exports = {
+    'simpleMethod': simpleMethod,
+    'greet' : greet,
+    'greetDeeplyCurried' : greetDeeplyCurried,
+    'greetDeeplyCurriedFatArrow': greetDeeplyCurriedFatArrow,
+    'greetCurried' : greetCurried,
+    'greeting' : greeting,
+    'curryIt' : curryIt
+};
---
+++ test/my_currying_test.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const myCurrying = require('../src/my_currying');
+
+describe ('I have a learning module for currying', function(){
+    it('a simple test for returning a string', function (){
+        expect(myCurrying.simpleMethod()).to.be.equal('paul');
+    });
+
+    it('calls greet function, return a greeting', function (){
+        expect(myCurrying.greet('hello', 'paul')).to.be.equal('hello, paul');
+    });
+
+    it('calls greet curried function, return a greeting', function (){
+        const greetHello = myCurrying.greetCurried('hello');
+        expect(greetHello('paul')).to.be.equal('hello, paul');
+        expect(greetHello('david')).to.be.equal('hello, david');
+        expect(myCurrying.greetCurried('GoodDay')('Paddy')).to.be.equal('GoodDay, Paddy');
+    });
+
+    it('calls deeply curried function, return a greeting', function(){
+        const greetAwkardly = myCurrying.greetDeeplyCurried('Hello')('....')('?');
+        expect(greetAwkardly('Ruby')).to.be.equal('Hello....Ruby?');
+
+    });
+
+    it('calls deeply curried fat arrow function, return a greeting', function(){
+        const greetAwkardly = myCurrying.greetDeeplyCurriedFatArrow('Hello')('....')('?');
+        expect(greetAwkardly('Ruby')).to.be.equal('Hello....Ruby?');
+
+        const sayHello = myCurrying.greetDeeplyCurriedFatArrow('Hello')(',');
+        expect(sayHello('.')('John')).to.be.equal('Hello,John.');
+
+        const askHello = sayHello('?')
+        expect(askHello('Eddie')).to.be.equal('Hello,Eddie?')
+    });
+
+    it('calls the curryIt function, return a greeting', function (){        
+        const greetHello = myCurrying.curryIt(myCurrying.greeting, "Hello", ", ", "."); 
+        expect(greetHello('Heidi')).to.be.equal('Hello, Heidi.');
+        expect(greetHello('Paul')).to.be.equal('Hello, Paul.');
+    });
+
+});
+
