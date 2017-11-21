0156d874345e20321864326587e8878b7c4dca6f
Paul Cavanagh <18164174+paulcavanagh@users.noreply.github.com>
2017-11-21 12:55:09 +0000

    Some shorthands research

---
+++ test/myShorthand_test.js
@@ -0,0 +1,41 @@
+/* 
+  12 Good JavaScript Shorthand Techniques
+  https://hackernoon.com/12-amazing-javascript-shorthand-techniques-fef16cdbc7fe
+*/
+
+'use strict'
+
+const chalk = require('chalk');
+
+describe.only(chalk.blue('12 Good JavaScript Shorthand Techniques'), () =>{
+  describe(chalk.magenta('Null, Undefined, Empty Checks Shorthand'), () => {
+    it('null value example', ()=>{
+     const var1 = null;
+     const var2 = var1 || '';
+     expect(var2).to.be.equal('');
+    });
+    it('undefined value example', () =>{
+      let variable1 = undefined;
+      let variable2 = variable1  || '';
+      expect(variable2).to.be.equal ('');
+    });
+    it('normal value example', () =>{
+      let variable1 = 'hi there';
+      let variable2 = variable1  || '';
+      expect(variable2).to.be.equal ('hi there');
+    });    
+  });
+  describe(chalk.magenta('Object Array Notation Shorthand'), () => {
+    it('Longhand', () => {
+      let a = new Array();
+      a[0] = 'paul';
+      a[1] = 'George';
+      a[2] = 'Conal';
+      expect(a.length).to.be.equal(3);
+    });
+    it('shorthand', ()=> {
+      let a = ['paul', 'George', 'Conal']
+      expect(a.length).to.be.equal(3);
+    });
+  });
+});
